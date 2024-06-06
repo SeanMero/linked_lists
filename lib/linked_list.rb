2d3607a5b347bcd68ffdd7f0bdaadbae5c9adad9
SeanMero <MeroFromVero@gmail.com>
2024-06-06 20:26:13 +0800

    Add functionality to identify the head node

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,22 +1,30 @@
 # contains methods for adding nodes a linked list
 class LinkedList
-  attr_accessor number_of_nodes
+  attr_accessor :number_of_nodes
 
   def initialize
+    @head = nil
     @number_of_nodes = 0
   end
 
   def append(value)
-    Node.new(value)
+    this = Node.new(value)
+    @head = this if head == nil
     @number_of_nodes += 1
   end
 
   def prepend(value)
-    Node.new(value)
+    this = Node.new(value)
+    this.next_node = head
+    @head = this
     @number_of_nodes += 1
   end
 
   def size
-    puts @number_of_nodes
+    puts number_of_nodes
+  end
+
+  def head
+    puts @head
   end
 end
